7a706a487c5a77d4459e167b91746d5980298037
iamWebLuk <lukas.weber@edu.fh-joanneum.at>
2021-05-02 19:32:47 +0200

    added ReadMe and First Chat Prototype

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Chat_App
+# ChatApp
+# Web Service Development
+
+## 4th Semester FH Joanneum ##
+
+# Group Project for the Course Web Service Development
+## David Riegler-Ulrike Ozim-Lukas Weber
+
+#### In this exercise we are programming a Chat App with Websocket Message Queuing and a Database
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+
+io.on("connection", (socket) => {
+    socket.on("chat message", msg => {
+        io.emit("chat message", msg);
+        console.log("message: " + msg);
+    });
+});
+
+http.listen(port, () => {
+    console.log("Server is running on Port: " + port);
+});
