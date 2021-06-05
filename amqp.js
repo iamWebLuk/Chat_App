a60d922e69abce2dabd67e134b3a1ebf5bbd4f0f
David Riegler <david.riegler@edu.fh-joanneum.at>
2021-06-05 21:40:41 +0200

    format code, add getUser logic, add tests

---
+++ amqp.js
@@ -0,0 +1,60 @@
+const amqp = require("amqplib/callback_api");
+const { RABBIT_HOST, QUEUE, EXCHANGE, KEY } = require("./config");
+
+function consumeMessage(emit) {
+  amqp.connect(`amqp://${RABBIT_HOST}`, (connectionError, connection) => {
+    if (connectionError) throw connectionError;
+
+    connection.createChannel((channelError, channel) => {
+      if (channelError) throw channelError;
+
+      channel.assertExchange(EXCHANGE, "direct", {
+        durable: false,
+      });
+
+      channel.assertQueue(QUEUE, {
+        exclusive: true,
+      });
+
+      channel.bindQueue(QUEUE, EXCHANGE, KEY);
+
+      channel.consume(
+        QUEUE,
+        (payload) => {
+          payload = JSON.parse(payload.content);
+          console.log("Consumed: " + JSON.stringify(payload) + ". Key: " + KEY);
+
+          if (payload.user != null) {
+            emit(payload);
+          }
+        },
+        {
+          noAck: true,
+        }
+      );
+    });
+  });
+}
+
+function publishMessage(payload) {
+  amqp.connect(`amqp://${RABBIT_HOST}`, (connectionError, connection) => {
+    if (connectionError) throw connectionError;
+
+    connection.createChannel((channelError, channel) => {
+      if (channelError) throw channelError;
+
+      channel.assertExchange(EXCHANGE, "direct", {
+        durable: false,
+      });
+
+      channel.publish(EXCHANGE, KEY, Buffer.from(JSON.stringify(payload)));
+
+      console.log("Sent: " + JSON.stringify(payload));
+    });
+  });
+}
+
+module.exports = {
+  consumeMessage,
+  publishMessage,
+};
